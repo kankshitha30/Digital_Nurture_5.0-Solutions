c229c26a7eeb7cda68d382590a69406606204272
kankshitha30 <kankshithagopalasetti2006@gmail.com>
2026-06-20 16:07:33 +0530

    Add files via upload
    Implementation of the Factory method example

---
+++ factorymethodpatternexample/FactoryMethodTest.java
@@ -0,0 +1,11 @@
+public class FactoryMethodTest{
+public static void main(String args[])
+{
+DocumentFactory wordFactory=new WordDocumentFactory();
+DocumentFactory pdfFactory=new PdfDocumentFactory();
+DocumentFactory excelFactory=new ExcelDocumentFactory();
+wordFactory.openDocument();
+pdfFactory.openDocument();
+excelFactory.openDocument();
+}
+}

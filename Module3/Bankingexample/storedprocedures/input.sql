1bae3f4c7a362aa97468f2771de56d41770bab34
kankshitha30 <kankshithagopalasetti2006@gmail.com>
2026-06-23 23:09:28 +0530

    Add files via upload
    Stored procedures exercise execution

---
+++ Module3/Bankingexample/storedprocedures/input.sql
@@ -0,0 +1,7 @@
+INSERT INTO Accounts VALUES (1001,'Savings',10000);
+INSERT INTO Accounts VALUES (1002,'Savings',6000);
+INSERT INTO Accounts VALUES (1003,'Current',30000);
+
+INSERT INTO Employees VALUES (01,'Aadya','IT',60000);
+INSERT INTO Employees VALUES (02,'Vividh','IT',40000);
+INSERT INTO Employees VALUES (03,'Sudha','HR',70000);

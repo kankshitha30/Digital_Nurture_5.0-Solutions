2f4c1f5a8eb2c6477c44f46b2234d5329d984dd9
kankshitha30 <kankshithagopalasetti2006@gmail.com>
2026-06-23 23:12:55 +0530

    Delete Module3/Bankingexample directory

--- Module3/Bankingexample/storedprocedures/input.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Accounts VALUES (1001,'Savings',10000);
-INSERT INTO Accounts VALUES (1002,'Savings',6000);
-INSERT INTO Accounts VALUES (1003,'Current',30000);
-
-INSERT INTO Employees VALUES (01,'Aadya','IT',60000);
-INSERT INTO Employees VALUES (02,'Vividh','IT',40000);
-INSERT INTO Employees VALUES (03,'Sudha','HR',70000);

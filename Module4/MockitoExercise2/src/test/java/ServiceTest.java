450d14933a3446506dc9c7c0403c0ff18090b80d
kankshitha30 <kankshithagopalasetti2006@gmail.com>
2026-07-02 21:15:41 +0530

    Add files via upload

---
+++ Module4/MockitoExercise2/src/test/java/ServiceTest.java
@@ -0,0 +1,13 @@
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+public class ServiceTest {
+    @Test
+    public void testVerifyInteraction() {
+        ExternalApi mockApi = Mockito.mock(ExternalApi.class);
+        Service service = new Service(mockApi);
+        service.fetchData();
+        verify(mockApi).getData();
+        System.out.println("Interaction is Verified Successfully");
+    }
+}

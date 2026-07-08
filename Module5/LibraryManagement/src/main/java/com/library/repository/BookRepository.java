1645d9bda3e53f10b08a291dfaa2c447d4dcf712
kankshitha30 <kankshithagopalasetti2006@gmail.com>
2026-07-08 22:34:26 +0530

    Add files via upload

---
+++ Module5/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    public void repositoryWork() {
+        System.out.println("BookRepository Bean is Working...");
+    }
+}
---
+++ Module5/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,8 @@
+package com.library.service;
+
+public class BookService {
+
+    public void serviceWork() {
+        System.out.println("BookService Bean is Working...");
+    }
+}

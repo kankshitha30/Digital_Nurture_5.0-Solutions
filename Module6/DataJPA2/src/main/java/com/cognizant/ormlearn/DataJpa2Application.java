9a6cb0ad27d5a282b2ab7a6207b0d97eab56302b
kankshitha30 <kankshithagopalasetti2006@gmail.com>
2026-07-16 21:21:00 +0530

    Add files via upload

---
+++ Module6/DataJPA2/src/main/java/com/cognizant/ormlearn/DataJpa2Application.java
@@ -0,0 +1,13 @@
+package com.cognizant.ormlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpa2Application.class, args);
+	}
+
+}

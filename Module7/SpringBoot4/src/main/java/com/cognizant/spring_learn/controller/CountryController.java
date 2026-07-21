6e104d5517170c6340c0deda7a8c219d3b4961cc
kankshitha30 <kankshithagopalasetti2006@gmail.com>
2026-07-21 23:56:37 +0530

    Add files via upload

---
+++ Module7/SpringBoot4/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -0,0 +1,20 @@
+package com.cognizant.spring_learn.controller;
+
+import com.cognizant.spring_learn.model.Country;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+
+        return context.getBean("in", Country.class);
+    }
+}
